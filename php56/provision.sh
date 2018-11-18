f0e94389eb9a0c0d6162f0478e6c912d6064f725
Tom J Nowell <contact@tomjn.com>
2018-11-18 15:03:47 +0000

    move the network checks in the PHP provisioners to a common.sh and replace them with the VVV core versions

--- php56/provision.sh
+++ php56/provision.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+../common.sh
+
 DIR=`dirname $0`
 
 # PACKAGE INSTALLATION
@@ -48,29 +50,6 @@ apt_package_check_list=(
 )
 
 ### FUNCTIONS
-
-network_detection() {
-  # Network Detection
-  #
-  # Make an HTTP request to google.com to determine if outside access is available
-  # to us. If 3 attempts with a timeout of 5 seconds are not successful, then we'll
-  # skip a few things further in provisioning rather than create a bunch of errors.
-  if [[ "$(wget --tries=3 --timeout=5 --spider --recursive --level=2 http://google.com 2>&1 | grep 'connected')" ]]; then
-    echo "Network connection detected..."
-    ping_result="Connected"
-  else
-    echo "Network connection not detected. Unable to reach google.com..."
-    ping_result="Not Connected"
-  fi
-}
-
-network_check() {
-  network_detection
-  if [[ ! "$ping_result" == "Connected" ]]; then
-    echo -e "\nNo network connection available, skipping package installation"
-    exit 0
-  fi
-}
 
 not_installed() {
   dpkg -s "$1" 2>&1 | grep -q 'Version:'
